e577480bacd559a2ee52b350d9ea773a298c7006
smessinis <stavros.messinis@gmail.com>
2023-03-31 04:41:58 +0300

    Create readstreamMQTT.py

---
+++ readstreamMQTT.py
@@ -0,0 +1,23 @@
+import paho.mqtt.client as mqtt
+
+# Define MQTT broker details
+broker_address = "localhost"
+broker_port = 1883
+topic = "my_topic"
+
+# Define callback functions
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code "+str(rc))
+    client.subscribe(topic)
+
+def on_message(client, userdata, msg):
+    print("Received message on topic "+msg.topic+": "+msg.payload.decode())
+
+# Create MQTT client and connect to broker
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+client.connect(broker_address, broker_port)
+
+# Start loop to listen for messages
+client.loop_forever()
